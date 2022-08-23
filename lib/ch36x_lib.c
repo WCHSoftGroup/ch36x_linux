f2c5051366fe4c1cfd0e2824fdbbee02de1bf90a
SoldierJazz <SoldierJazz@163.com>
2022-08-23 15:07:23 +0800

    V1.22 Release
    fixed bugs in spi transfer in

--- lib/ch36x_lib.c
+++ lib/ch36x_lib.c
@@ -1,8 +1,9 @@
 /*
  * ch365/ch367/ch368 PCI/PCIE application library
  *
- * Copyright (C) 2021 WCH.
- * Author: TECH39 <zhangj@wch.cn>
+ * Copyright (C) 2022 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Web: http://wch.cn
+ * Author: WCH@TECH39 <tech@wch.cn>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -11,24 +12,24 @@
  * Cross-compile with cross-gcc -I /path/to/cross-kernel/include
  *
  * Version: V1.11
- * 
+ *
  * Update Log:
  * V1.0 - initial version
  * V1.1 - modified APIs related to interrupt
  * V1.11 - added APIs related to SPI transfer
  */
- 
+
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <fcntl.h>  
-#include <errno.h>   
 #include <string.h>
-#include <sys/types.h> 
-#include <sys/stat.h>
 #include <sys/ioctl.h>
-#include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "ch36x_lib.h"
 
 static const char *device = "/dev/ch36xpci0";
@@ -41,17 +42,17 @@ static const char *device = "/dev/ch36xpci0";
  */
 int ch36x_open(const char *devname)
 {
-	int fd;
+    int fd;
 
-	fd = open(devname, O_RDWR);
-	if (fd > 0) {
-		fcntl(fd, F_SETOWN, getpid());
-		fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | FASYNC);
-	}
+    fd = open(devname, O_RDWR);
+    if (fd > 0) {
+        fcntl(fd, F_SETOWN, getpid());
+        fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | FASYNC);
+    }
 
-	return fd;
+    return fd;
 }
- 
+
 /**
  * ch36x_close - close ch36x device
  * @fd: the device handle
@@ -60,7 +61,7 @@ int ch36x_open(const char *devname)
  */
 int ch36x_close(int fd)
 {
-	return close(fd);
+    return close(fd);
 }
 
 /**
@@ -72,7 +73,7 @@ int ch36x_close(int fd)
  */
 int ch36x_get_chiptype(int fd, enum CHIP_TYPE *chiptype)
 {
-	return ioctl(fd, CH36x_GET_CHIPTYPE, (unsigned long)chiptype);
+    return ioctl(fd, CH36x_GET_CHIPTYPE, (unsigned long)chiptype);
 }
 
 /**
@@ -84,7 +85,7 @@ int ch36x_get_chiptype(int fd, enum CHIP_TYPE *chiptype)
  */
 int ch36x_get_version(int fd, char *version)
 {
-	return ioctl(fd, CH36x_GET_VERSION, (unsigned long)version);
+    return ioctl(fd, CH36x_GET_VERSION, (unsigned long)version);
 }
 
 /**
@@ -96,7 +97,7 @@ int ch36x_get_version(int fd, char *version)
  */
 int ch36x_get_ioaddr(int fd, void *ioaddr)
 {
-	return ioctl(fd, CH36x_GET_IO_BASE_ADDR, (unsigned long)ioaddr);
+    return ioctl(fd, CH36x_GET_IO_BASE_ADDR, (unsigned long)ioaddr);
 }
 
 /**
@@ -109,7 +110,7 @@ int ch36x_get_ioaddr(int fd, void *ioaddr)
 
 int ch36x_get_memaddr(int fd, void *memaddr)
 {
-	return ioctl(fd, CH36x_GET_MEM_BASE_ADDR, (unsigned long)memaddr);
+    return ioctl(fd, CH36x_GET_MEM_BASE_ADDR, (unsigned long)memaddr);
 }
 
 /**
@@ -122,15 +123,15 @@ int ch36x_get_memaddr(int fd, void *memaddr)
  */
 int ch36x_read_config_byte(int fd, uint8_t offset, uint8_t *obyte)
 {
-	struct {
-		uint8_t offset;
-		uint8_t *obyte;
-	} ch36x_read_config_t;
-	
-	ch36x_read_config_t.offset = offset;
-	ch36x_read_config_t.obyte = obyte;
+    struct {
+        uint8_t offset;
+        uint8_t *obyte;
+    } ch36x_read_config_t;
 
-	return ioctl(fd, CH36x_READ_CONFIG_BYTE, (unsigned long)&ch36x_read_config_t);
+    ch36x_read_config_t.offset = offset;
+    ch36x_read_config_t.obyte = obyte;
+
+    return ioctl(fd, CH36x_READ_CONFIG_BYTE, (unsigned long)&ch36x_read_config_t);
 }
 
 /**
@@ -143,15 +144,15 @@ int ch36x_read_config_byte(int fd, uint8_t offset, uint8_t *obyte)
  */
 int ch36x_read_config_word(int fd, uint8_t offset, uint16_t *oword)
 {
-	struct {
-		uint8_t offset;
-		uint16_t *oword;
-	} ch36x_read_config_t;
-	
-	ch36x_read_config_t.offset = offset;
-	ch36x_read_config_t.oword = oword;
+    struct {
+        uint8_t offset;
+        uint16_t *oword;
+    } ch36x_read_config_t;
 
-	return ioctl(fd, CH36x_READ_CONFIG_WORD, (unsigned long)&ch36x_read_config_t);
+    ch36x_read_config_t.offset = offset;
+    ch36x_read_config_t.oword = oword;
+
+    return ioctl(fd, CH36x_READ_CONFIG_WORD, (unsigned long)&ch36x_read_config_t);
 }
 
 /**
@@ -164,15 +165,15 @@ int ch36x_read_config_word(int fd, uint8_t offset, uint16_t *oword)
  */
 int ch36x_read_config_dword(int fd, uint8_t offset, uint32_t *odword)
 {
-	struct {
-		uint8_t offset;
-		uint32_t *odword;
-	} ch36x_read_config_t;
-	
-	ch36x_read_config_t.offset = offset;
-	ch36x_read_config_t.odword = odword;
+    struct {
+        uint8_t offset;
+        uint32_t *odword;
+    } ch36x_read_config_t;
 
-	return ioctl(fd, CH36x_READ_CONFIG_DWORD, (unsigned long)&ch36x_read_config_t);
+    ch36x_read_config_t.offset = offset;
+    ch36x_read_config_t.odword = odword;
+
+    return ioctl(fd, CH36x_READ_CONFIG_DWORD, (unsigned long)&ch36x_read_config_t);
 }
 
 /**
@@ -185,15 +186,15 @@ int ch36x_read_config_dword(int fd, uint8_t offset, uint32_t *odword)
  */
 int ch36x_write_config_byte(int fd, uint8_t offset, uint8_t ibyte)
 {
-	struct {
-		uint8_t offset;
-		uint8_t ibyte;
-	} ch36x_write_config_t;
-	
-	ch36x_write_config_t.offset = offset;
-	ch36x_write_config_t.ibyte = ibyte;
+    struct {
+        uint8_t offset;
+        uint8_t ibyte;
+    } ch36x_write_config_t;
 
-	return ioctl(fd, CH36x_WRITE_CONFIG_BYTE, (unsigned long)&ch36x_write_config_t);
+    ch36x_write_config_t.offset = offset;
+    ch36x_write_config_t.ibyte = ibyte;
+
+    return ioctl(fd, CH36x_WRITE_CONFIG_BYTE, (unsigned long)&ch36x_write_config_t);
 }
 
 /**
@@ -206,15 +207,15 @@ int ch36x_write_config_byte(int fd, uint8_t offset, uint8_t ibyte)
  */
 int ch36x_write_config_word(int fd, uint8_t offset, uint16_t iword)
 {
-	struct {
-		uint8_t offset;
-		uint16_t iword;
-	} ch36x_write_config_t;
-	
-	ch36x_write_config_t.offset = offset;
-	ch36x_write_config_t.iword = iword;
+    struct {
+        uint8_t offset;
+        uint16_t iword;
+    } ch36x_write_config_t;
 
-	return ioctl(fd, CH36x_WRITE_CONFIG_WORD, (unsigned long)&ch36x_write_config_t);
+    ch36x_write_config_t.offset = offset;
+    ch36x_write_config_t.iword = iword;
+
+    return ioctl(fd, CH36x_WRITE_CONFIG_WORD, (unsigned long)&ch36x_write_config_t);
 }
 
 /**
@@ -227,15 +228,15 @@ int ch36x_write_config_word(int fd, uint8_t offset, uint16_t iword)
  */
 int ch36x_write_config_dword(int fd, uint8_t offset, uint32_t idword)
 {
-	struct {
-		uint8_t offset;
-		uint32_t idword;
-	} ch36x_write_config_t;
-	
-	ch36x_write_config_t.offset = offset;
-	ch36x_write_config_t.idword = idword;
+    struct {
+        uint8_t offset;
+        uint32_t idword;
+    } ch36x_write_config_t;
 
-	return ioctl(fd, CH36x_WRITE_CONFIG_DWORD, (unsigned long)&ch36x_write_config_t);
+    ch36x_write_config_t.offset = offset;
+    ch36x_write_config_t.idword = idword;
+
+    return ioctl(fd, CH36x_WRITE_CONFIG_DWORD, (unsigned long)&ch36x_write_config_t);
 }
 
 /**
@@ -248,15 +249,15 @@ int ch36x_write_config_dword(int fd, uint8_t offset, uint32_t idword)
  */
 int ch36x_read_io_byte(int fd, unsigned long ioaddr, uint8_t *obyte)
 {
-	struct {
-		unsigned long ioaddr;
-		uint8_t *obyte;
-	} ch36x_read_io_t;
-	
-	ch36x_read_io_t.ioaddr = ioaddr;
-	ch36x_read_io_t.obyte = obyte;
+    struct {
+        unsigned long ioaddr;
+        uint8_t *obyte;
+    } ch36x_read_io_t;
 
-	return ioctl(fd, CH36x_READ_IO_BYTE, (unsigned long)&ch36x_read_io_t);
+    ch36x_read_io_t.ioaddr = ioaddr;
+    ch36x_read_io_t.obyte = obyte;
+
+    return ioctl(fd, CH36x_READ_IO_BYTE, (unsigned long)&ch36x_read_io_t);
 }
 
 /**
@@ -269,15 +270,15 @@ int ch36x_read_io_byte(int fd, unsigned long ioaddr, uint8_t *obyte)
  */
 int ch36x_read_io_word(int fd, unsigned long ioaddr, uint16_t *oword)
 {
-	struct {
-		unsigned long ioaddr;
-		uint16_t *oword;
-	} ch36x_read_io_t;
-	
-	ch36x_read_io_t.ioaddr = ioaddr;
-	ch36x_read_io_t.oword = oword;
+    struct {
+        unsigned long ioaddr;
+        uint16_t *oword;
+    } ch36x_read_io_t;
 
-	return ioctl(fd, CH36x_READ_IO_WORD, (unsigned long)&ch36x_read_io_t);
+    ch36x_read_io_t.ioaddr = ioaddr;
+    ch36x_read_io_t.oword = oword;
+
+    return ioctl(fd, CH36x_READ_IO_WORD, (unsigned long)&ch36x_read_io_t);
 }
 
 /**
@@ -290,15 +291,15 @@ int ch36x_read_io_word(int fd, unsigned long ioaddr, uint16_t *oword)
  */
 int ch36x_read_io_dword(int fd, unsigned long ioaddr, uint32_t *odword)
 {
-	struct {
-		unsigned long ioaddr;
-		uint32_t *odword;
-	} ch36x_read_io_t;
-	
-	ch36x_read_io_t.ioaddr = ioaddr;
-	ch36x_read_io_t.odword = odword;
+    struct {
+        unsigned long ioaddr;
+        uint32_t *odword;
+    } ch36x_read_io_t;
 
-	return ioctl(fd, CH36x_READ_IO_DWORD, (unsigned long)&ch36x_read_io_t);
+    ch36x_read_io_t.ioaddr = ioaddr;
+    ch36x_read_io_t.odword = odword;
+
+    return ioctl(fd, CH36x_READ_IO_DWORD, (unsigned long)&ch36x_read_io_t);
 }
 
 /**
@@ -311,15 +312,15 @@ int ch36x_read_io_dword(int fd, unsigned long ioaddr, uint32_t *odword)
  */
 int ch36x_write_io_byte(int fd, unsigned long ioaddr, uint8_t ibyte)
 {
-	struct {
-		unsigned long ioaddr;
-		uint8_t ibyte;
-	} ch36x_write_io_t;
-	
-	ch36x_write_io_t.ioaddr = ioaddr;
-	ch36x_write_io_t.ibyte = ibyte;
+    struct {
+        unsigned long ioaddr;
+        uint8_t ibyte;
+    } ch36x_write_io_t;
 
-	return ioctl(fd, CH36x_WRITE_IO_BYTE, (unsigned long)&ch36x_write_io_t);
+    ch36x_write_io_t.ioaddr = ioaddr;
+    ch36x_write_io_t.ibyte = ibyte;
+
+    return ioctl(fd, CH36x_WRITE_IO_BYTE, (unsigned long)&ch36x_write_io_t);
 }
 
 /**
@@ -332,15 +333,15 @@ int ch36x_write_io_byte(int fd, unsigned long ioaddr, uint8_t ibyte)
  */
 int ch36x_write_io_word(int fd, unsigned long ioaddr, uint16_t iword)
 {
-	struct {
-		unsigned long ioaddr;
-		uint16_t iword;
-	} ch36x_write_io_t;
-	
-	ch36x_write_io_t.ioaddr = ioaddr;
-	ch36x_write_io_t.iword = iword;
+    struct {
+        unsigned long ioaddr;
+        uint16_t iword;
+    } ch36x_write_io_t;
 
-	return ioctl(fd, CH36x_WRITE_IO_WORD, (unsigned long)&ch36x_write_io_t);
+    ch36x_write_io_t.ioaddr = ioaddr;
+    ch36x_write_io_t.iword = iword;
+
+    return ioctl(fd, CH36x_WRITE_IO_WORD, (unsigned long)&ch36x_write_io_t);
 }
 
 /**
@@ -353,15 +354,15 @@ int ch36x_write_io_word(int fd, unsigned long ioaddr, uint16_t iword)
  */
 int ch36x_write_io_dword(int fd, unsigned long ioaddr, uint32_t idword)
 {
-	struct {
-		unsigned long ioaddr;
-		uint32_t idword;
-	} ch36x_write_io_t;
-	
-	ch36x_write_io_t.ioaddr = ioaddr;
-	ch36x_write_io_t.idword = idword;
+    struct {
+        unsigned long ioaddr;
+        uint32_t idword;
+    } ch36x_write_io_t;
 
-	return ioctl(fd, CH36x_WRITE_IO_DWORD, (unsigned long)&ch36x_write_io_t);
+    ch36x_write_io_t.ioaddr = ioaddr;
+    ch36x_write_io_t.idword = idword;
+
+    return ioctl(fd, CH36x_WRITE_IO_DWORD, (unsigned long)&ch36x_write_io_t);
 }
 
 /**
@@ -374,15 +375,15 @@ int ch36x_write_io_dword(int fd, unsigned long ioaddr, uint32_t idword)
  */
 int ch36x_read_mem_byte(int fd, unsigned long memaddr, uint8_t *obyte)
 {
-	struct {
-		unsigned long memaddr;
-		uint8_t *obyte;
-	} ch36x_read_mem_t;
-	
-	ch36x_read_mem_t.memaddr = memaddr;
-	ch36x_read_mem_t.obyte = obyte;
+    struct {
+        unsigned long memaddr;
+        uint8_t *obyte;
+    } ch36x_read_mem_t;
 
-	return ioctl(fd, CH36x_READ_MEM_BYTE, (unsigned long)&ch36x_read_mem_t);
+    ch36x_read_mem_t.memaddr = memaddr;
+    ch36x_read_mem_t.obyte = obyte;
+
+    return ioctl(fd, CH36x_READ_MEM_BYTE, (unsigned long)&ch36x_read_mem_t);
 }
 
 /**
@@ -395,15 +396,15 @@ int ch36x_read_mem_byte(int fd, unsigned long memaddr, uint8_t *obyte)
  */
 int ch36x_read_mem_word(int fd, unsigned long memaddr, uint16_t *oword)
 {
-	struct {
-		unsigned long memaddr;
-		uint16_t *oword;
-	} ch36x_read_mem_t;
-	
-	ch36x_read_mem_t.memaddr = memaddr;
-	ch36x_read_mem_t.oword = oword;
+    struct {
+        unsigned long memaddr;
+        uint16_t *oword;
+    } ch36x_read_mem_t;
 
-	return ioctl(fd, CH36x_READ_MEM_WORD, (unsigned long)&ch36x_read_mem_t);
+    ch36x_read_mem_t.memaddr = memaddr;
+    ch36x_read_mem_t.oword = oword;
+
+    return ioctl(fd, CH36x_READ_MEM_WORD, (unsigned long)&ch36x_read_mem_t);
 }
 
 /**
@@ -416,15 +417,15 @@ int ch36x_read_mem_word(int fd, unsigned long memaddr, uint16_t *oword)
  */
 int ch36x_read_mem_dword(int fd, unsigned long memaddr, uint32_t *odword)
 {
-	struct {
-		unsigned long memaddr;
-		uint32_t *odword;
-	} ch36x_read_mem_t;
-	
-	ch36x_read_mem_t.memaddr = memaddr;
-	ch36x_read_mem_t.odword = odword;
+    struct {
+        unsigned long memaddr;
+        uint32_t *odword;
+    } ch36x_read_mem_t;
 
-	return ioctl(fd, CH36x_READ_MEM_DWORD, (unsigned long)&ch36x_read_mem_t);
+    ch36x_read_mem_t.memaddr = memaddr;
+    ch36x_read_mem_t.odword = odword;
+
+    return ioctl(fd, CH36x_READ_MEM_DWORD, (unsigned long)&ch36x_read_mem_t);
 }
 
 /**
@@ -437,15 +438,15 @@ int ch36x_read_mem_dword(int fd, unsigned long memaddr, uint32_t *odword)
  */
 int ch36x_write_mem_byte(int fd, unsigned long memaddr, uint8_t ibyte)
 {
-	struct {
-		unsigned long memaddr;
-		uint8_t ibyte;
-	} ch36x_write_mem_t;
-	
-	ch36x_write_mem_t.memaddr = memaddr;
-	ch36x_write_mem_t.ibyte = ibyte;
+    struct {
+        unsigned long memaddr;
+        uint8_t ibyte;
+    } ch36x_write_mem_t;
 
-	return ioctl(fd, CH36x_WRITE_MEM_BYTE, (unsigned long)&ch36x_write_mem_t);
+    ch36x_write_mem_t.memaddr = memaddr;
+    ch36x_write_mem_t.ibyte = ibyte;
+
+    return ioctl(fd, CH36x_WRITE_MEM_BYTE, (unsigned long)&ch36x_write_mem_t);
 }
 
 /**
@@ -458,15 +459,15 @@ int ch36x_write_mem_byte(int fd, unsigned long memaddr, uint8_t ibyte)
  */
 int ch36x_write_mem_word(int fd, unsigned long memaddr, uint16_t iword)
 {
-	struct {
-		unsigned long memaddr;
-		uint16_t iword;
-	} ch36x_write_mem_t;
-	
-	ch36x_write_mem_t.memaddr = memaddr;
-	ch36x_write_mem_t.iword = iword;
+    struct {
+        unsigned long memaddr;
+        uint16_t iword;
+    } ch36x_write_mem_t;
 
-	return ioctl(fd, CH36x_WRITE_MEM_WORD, (unsigned long)&ch36x_write_mem_t);
+    ch36x_write_mem_t.memaddr = memaddr;
+    ch36x_write_mem_t.iword = iword;
+
+    return ioctl(fd, CH36x_WRITE_MEM_WORD, (unsigned long)&ch36x_write_mem_t);
 }
 
 /**
@@ -479,15 +480,15 @@ int ch36x_write_mem_word(int fd, unsigned long memaddr, uint16_t iword)
  */
 int ch36x_write_mem_dword(int fd, unsigned long memaddr, uint32_t idword)
 {
-	struct {
-		unsigned long memaddr;
-		uint32_t idword;
-	} ch36x_write_mem_t;
-	
-	ch36x_write_mem_t.memaddr = memaddr;
-	ch36x_write_mem_t.idword = idword;
+    struct {
+        unsigned long memaddr;
+        uint32_t idword;
+    } ch36x_write_mem_t;
 
-	return ioctl(fd, CH36x_WRITE_MEM_DWORD, (unsigned long)&ch36x_write_mem_t);
+    ch36x_write_mem_t.memaddr = memaddr;
+    ch36x_write_mem_t.idword = idword;
+
+    return ioctl(fd, CH36x_WRITE_MEM_DWORD, (unsigned long)&ch36x_write_mem_t);
 }
 
 /**
@@ -501,20 +502,20 @@ int ch36x_write_mem_dword(int fd, unsigned long memaddr, uint32_t idword)
  */
 int ch36x_read_mem_block(int fd, unsigned long memaddr, uint8_t *obuffer, unsigned long len)
 {
-	struct {
-		unsigned long memaddr;
-		uint8_t *obuffer;
-		unsigned long len;
-	} ch36x_read_mem_t;
-	
-	if ((len <= 0) || (len > sizeof(mCH368_MEM_REG))) {
-		return -1;
-	}
-	ch36x_read_mem_t.memaddr = memaddr;
-	ch36x_read_mem_t.obuffer = obuffer;
-	ch36x_read_mem_t.len = len;
+    struct {
+        unsigned long memaddr;
+        uint8_t *obuffer;
+        unsigned long len;
+    } ch36x_read_mem_t;
 
-	return ioctl(fd, CH36x_READ_MEM_BLOCK, (unsigned long)&ch36x_read_mem_t);
+    if ((len <= 0) || (len > sizeof(mCH368_MEM_REG))) {
+        return -1;
+    }
+    ch36x_read_mem_t.memaddr = memaddr;
+    ch36x_read_mem_t.obuffer = obuffer;
+    ch36x_read_mem_t.len = len;
+
+    return ioctl(fd, CH36x_READ_MEM_BLOCK, (unsigned long)&ch36x_read_mem_t);
 }
 
 /**
@@ -528,20 +529,20 @@ int ch36x_read_mem_block(int fd, unsigned long memaddr, uint8_t *obuffer, unsign
  */
 int ch36x_write_mem_block(int fd, unsigned long memaddr, uint8_t *ibuffer, unsigned long len)
 {
-	struct {
-		unsigned long memaddr;
-		uint8_t *ibuffer;
-		unsigned long len;
-	} ch36x_write_mem_t;
-	
-	if ((len <= 0) || (len > sizeof(mCH368_MEM_REG))) {
-		return -1;
-	}
-	ch36x_write_mem_t.memaddr = memaddr;
-	ch36x_write_mem_t.ibuffer = ibuffer;
-	ch36x_write_mem_t.len = len;
+    struct {
+        unsigned long memaddr;
+        uint8_t *ibuffer;
+        unsigned long len;
+    } ch36x_write_mem_t;
 
-	return ioctl(fd, CH36x_WRITE_MEM_BLOCK, (unsigned long)&ch36x_write_mem_t);
+    if ((len <= 0) || (len > sizeof(mCH368_MEM_REG))) {
+        return -1;
+    }
+    ch36x_write_mem_t.memaddr = memaddr;
+    ch36x_write_mem_t.ibuffer = ibuffer;
+    ch36x_write_mem_t.len = len;
+
+    return ioctl(fd, CH36x_WRITE_MEM_BLOCK, (unsigned long)&ch36x_write_mem_t);
 }
 
 /**
@@ -553,7 +554,7 @@ int ch36x_write_mem_block(int fd, unsigned long memaddr, uint8_t *ibuffer, unsig
  */
 int ch36x_enable_isr(int fd, enum INTMODE mode)
 {
-	return ioctl(fd, CH36x_ENABLE_INT, (unsigned long)&mode);
+    return ioctl(fd, CH36x_ENABLE_INT, (unsigned long)&mode);
 }
 
 /**
@@ -564,20 +565,20 @@ int ch36x_enable_isr(int fd, enum INTMODE mode)
  */
 int ch36x_disable_isr(int fd)
 {
-	return ioctl(fd, CH36x_DISABLE_INT, NULL);
+    return ioctl(fd, CH36x_DISABLE_INT, NULL);
 }
 
 /**
- * ch36x_set_int_routine - set interrupt handler 
+ * ch36x_set_int_routine - set interrupt handler
  * @fd: file descriptor of ch36x device
  * @isr_handler: handler to call when interrupt occurs
  *
  */
 void ch36x_set_int_routine(int fd, void *isr_handler)
 {
-	if (isr_handler != NULL) {
-		signal(SIGIO, isr_handler); 
-	}
+    if (isr_handler != NULL) {
+        signal(SIGIO, isr_handler);
+    }
 }
 
 /**
@@ -590,7 +591,7 @@ void ch36x_set_int_routine(int fd, void *isr_handler)
  */
 int ch36x_set_stream(int fd, unsigned long mode)
 {
-	return ioctl(fd, CH36x_SET_STREAM, (unsigned long)&mode);
+    return ioctl(fd, CH36x_SET_STREAM, (unsigned long)&mode);
 }
 
 /**
@@ -604,23 +605,23 @@ int ch36x_set_stream(int fd, unsigned long mode)
  */
 int ch36x_stream_spi(int fd, uint8_t *ibuffer, unsigned long ilen, uint8_t *obuffer, unsigned long olen)
 {
-	struct {
-		uint8_t *ibuffer;
-		unsigned long ilen;
-		uint8_t *obuffer;
-		unsigned long olen;
-	} ch36x_stream_spi_t;
-	
-	if ((ilen < 0) || (ilen > mMAX_BUFFER_LENGTH)) {
-		return -1;
-	}
-	if ((olen < 0) || (olen > mMAX_BUFFER_LENGTH)) {
-		return -1;
-	}
-	ch36x_stream_spi_t.ibuffer = ibuffer;
-	ch36x_stream_spi_t.ilen = ilen;
-	ch36x_stream_spi_t.obuffer = obuffer;
-	ch36x_stream_spi_t.olen = olen;
+    struct {
+        uint8_t *ibuffer;
+        unsigned long ilen;
+        uint8_t *obuffer;
+        unsigned long olen;
+    } ch36x_stream_spi_t;
 
-	return ioctl(fd, CH36x_STREAM_SPI, (unsigned long)&ch36x_stream_spi_t);
+    if ((ilen < 0) || (ilen > mMAX_BUFFER_LENGTH)) {
+        return -1;
+    }
+    if ((olen < 0) || (olen > mMAX_BUFFER_LENGTH)) {
+        return -1;
+    }
+    ch36x_stream_spi_t.ibuffer = ibuffer;
+    ch36x_stream_spi_t.ilen = ilen;
+    ch36x_stream_spi_t.obuffer = obuffer;
+    ch36x_stream_spi_t.olen = olen;
+
+    return ioctl(fd, CH36x_STREAM_SPI, (unsigned long)&ch36x_stream_spi_t);
 }
